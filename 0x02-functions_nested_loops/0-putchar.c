167cb3431000a8a45c8f0d63853a82af64c29dcb
itsmeabela <abelrobel7@gmail.com>
2022-03-15 07:57:42 -0700

    correct

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  **/
 int main(void)
 {
-	char ermias[] = "_putchar";
+	char abel[] = "_putchar";
 
 	int c;
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(ermias[c]);
+		_putchar(abel[c]);
 	}
 	_putchar('\n');
 	return (0);
