4916172796bbb7ab3ee7c3a670685adcca84c618
itsmeabela <abelrobel7@gmail.com>
2022-03-15 23:46:47 -0700

    lets check

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,50 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the times table
- * @n: takes in an integer
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
  */
+
 void print_times_table(int n)
 {
-  int i, j, k;
+	int row;
+	int column;
+	int product;
 
-  if (n > 15 || n < 0)
-    return;
-  for (i = 0; i <= n; i++)
-  {
-    for (j = 0; j <= n; j++)
-    {
-      k = i * j;
-      if (j == 0)
-      {
-        _putchar(k + '0');
-      }
-      else if (k <= 9)
-      {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(k + '0');
-      }
-      else if (k >= 10 && k < 100)
-      {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(k / 10 + '0');
-        _putchar(k % 10 + '0');
-      }
-      else
-      {
-        _putchar(',');
-        _putchar(' ');
-        _putchar((k / 100) + '0');
-        _putchar(k / 10 % 10 + '0');
-        _putchar(k % 10 + '0');
-      }
-    }
-    _putchar('\n');
-  }
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
